bf33629231c26e92a4bd8fbed6328979a5fa390d
cyrilrock <power90xl@gmail.com>
2014-05-02 15:35:38 -0400

    Final. Working Code.

--- CS3810Assig4/src/ExternalHashing.java
+++ CS3810Assig4/src/ExternalHashing.java
@@ -1,7 +1,13 @@
 import java.io.IOException;
 import java.util.*;
 import java.io.*;
-
+/**
+ * @author Cyril Thomas
+ * @since 2014-05-02
+ * <h3>CS3810 Assignment 4 External Hashing</h3> 
+ * <h1>Class: ExternalHashing </h1>
+ * <p>Main App Class to Create RandomAccessFile and test IndexHashTable Class</p>
+ */
 public class ExternalHashing {
 
 	public static void main(String[] args) throws IOException 
@@ -10,6 +16,9 @@ public class ExternalHashing {
 		int inputBlock = 0, inputRecord = 0;
 		RandomAccessFile f = null;
 		
+		/**
+		 * Create Random Access File known as "f"
+		 */
 		try 
 		{
 			f = new RandomAccessFile("newfile.txt", "rw");
@@ -21,7 +30,10 @@ public class ExternalHashing {
 		}
 		
 		
-		
+		/**
+		 * Ask User input for Block Size and Record 
+		 * both are Integer Input. 
+		 */
 		try
 		{	
 			System.out.print("Enter the Block Size: ");
@@ -35,23 +47,41 @@ public class ExternalHashing {
 			scan.nextLine();
 		}
 		
-		try {
-			Record tempCompareRec = new Record();
+		/**
+		 * Initiating the empty file with "DDD.." using for loop
+		 * with control as inputBlock*inputRecord. 
+		 */
+		try 
+		{
+			Record tempFill = new Record("DDDDDDDDDDDDDDDD","DDDD");
 			
 			for (int i =0 ; i<=(inputBlock*inputRecord); i++)
-				tempCompareRec.write(f);
-		} catch (Exception e) {
+				tempFill.write(f);
+		} 
+		catch (Exception e) 
+		{
 			e.printStackTrace();
 		}
 		
+		/**
+		 * Initiating IndexHashTable data using inputBlock and inputRecord
+		 */
 		IndexHashTable data = new IndexHashTable(inputBlock, inputRecord);
-				
+		
+		/**
+		 * Using switch Case
+		 * (0)Quit
+		 * (1)Insert
+		 * (2)Search
+		 * (3)Delete
+		 */
 		try 
 		{
 			do 
 			{
 				System.out.println("\nEnter: 1 to Insert ");
 				System.out.println("Enter: 2 to Search ");
+				System.out.println("Enter: 3 to Delete ");
 				System.out.println("Enter: 0 to Quit ");
 				System.out.print("Enter: ");
 				int input = scan.nextInt();
@@ -84,8 +114,8 @@ public class ExternalHashing {
 					{
 						scan.nextLine();
 						
-						System.out.print("\nEnter Student ID: ");
-						int key = scan.nextInt();
+						System.out.print("\nEnter Student ID to search: ");
+						String key = scan.nextLine();
 						
 						String message = data.search(f, key);
 						
@@ -93,6 +123,17 @@ public class ExternalHashing {
 						
 						break;
 					}
+					
+					case 3:
+					{
+						scan.nextLine();
+						System.out.print("\nEnter Student ID to delete: ");
+						String key = scan.nextLine();
+						String message = data.delete(f, key);
+						System.out.println(message);
+						break;
+					}
+					
 					default:
 					{
 						System.exit(0);
--- CS3810Assig4/src/Record.java
+++ CS3810Assig4/src/Record.java
@@ -1,9 +1,16 @@
-/*
- * Cyril Thomas
- * */
 import java.util.*;
 import java.io.*;
+/**
+ * <h3>Title:</h3> CS3810 Assignment 4 External Hashing
+ * @author [Prepared by the instructor]
+ * <h1>Record class</h1>
+ *	<p2>This class defines a record which is specifically for storing in
+ *   a Java random access file. See API of RandomAccessFile class in Java.</p2>
 
+ * <h2>Attributes:</h2>
+ *	<p3>name - String, the max length is 16 chars
+ *	id - String, the number of digits in id is 4.</p3>
+ */
 public class Record 
 {
 	private String name;
@@ -11,57 +18,102 @@ public class Record
 	private int NAMELENGTH=16;
 	private int IDLENGTH=4;
 	
-	
+	/**
+	 * default constructor
+	 */
 	public Record()
 	{
 		name=" ";
 		id=" ";
 	}
 	
+	/**
+	 * 
+	 * @param newName : String input for name.
+	 * @param newId	  : String input for id.
+	 * <h5>Description:</h5>
+	 * Read a 'name' and the related 'id' from the current position of
+	 * the given file to the instance variables 'name' and 'id'
+	 */
 	public Record(String newName, String newId)
 	{
 		name= newName;
 		id= newId;
 	}
 	
-	public void read(RandomAccessFile file) //throws IOException
+	/**
+	 * 
+	 * @param file : RandomAccessFile pass through
+	 * @throws IOException : If not able to read the pass through Random Access File
+	 * <h5>Description:</h5>
+	 * Write the value of 'this' object to the given file
+	 */
+	public void read(RandomAccessFile file) throws IOException
 	{
 		name= readString(file, NAMELENGTH);
 		id = readString(file, IDLENGTH);
 	}
 	
-	public void write(RandomAccessFile file) //throws IOException
+	/**
+	 * 
+	 * @param file : RandomAccessFile pass through
+	 * @throws IOException : If not able to read the pass through Random Access File
+	 * <h5>Description:</h5>
+	 * Write the value of 'this' object to the given file
+	 */
+	public void write(RandomAccessFile file) throws IOException
 	{
 		try {
 			writeStr(file, name, NAMELENGTH);
 			writeStr(file, id, IDLENGTH);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 		
 	}
 	
+	/**
+	 * 
+	 * @return String id
+	 */
 	public String getId()
 	{
 		return id;
 	}
 	
+	/**
+	 * 
+	 * @return String name
+	 */
 	public String getName()
 	{
 		return name;
 	}
 	
-	private String readString(RandomAccessFile file, int strLength) //throws IOException
+	/**
+	 * 
+	 * @param file : Pass Through Random Access File
+	 * @param strLength : length of the String as Integer
+	 * @return String theStr
+	 * @throws IOException : If failed to read the file
+	 * <h5>Description:</h5>
+	 * Help method: read a string with length of strLength from the file
+	 * Return the string to the caller
+	 */
+	private String readString(RandomAccessFile file, int strLength) throws IOException
 	{
 		char[] chs = new char[strLength];
 		
 		for (int i = 0; i < chs.length; i++)
 		{
-			try {
+			try 
+			{
 				chs[i] = file.readChar();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
+			} 
+			catch (IOException e) 
+			{
+				
 				e.printStackTrace();
 			}
 		}
@@ -70,6 +122,15 @@ public class Record
 		return theStr;
 	}
 	
+	/**
+	 * 
+	 * @param file : Pass Through Random Access File
+	 * @param str : String 
+	 * @param strLength : String Length
+	 * @throws IOException : If unable to write. 
+	 * <h5>Description:</h5>
+	 * Help method: write a string with length of strLength to the file
+	 */
 	private void writeStr(RandomAccessFile file, String str, int strLength) throws IOException
 	{
 		StringBuffer buffer = null;
@@ -83,6 +144,9 @@ public class Record
 		file.writeChars(buffer.toString());
 	}
 	
+	/**
+	 * @return String name & id
+	 */
 	public String toString()
 	{
 		return "\nName: " + name + "\nID: " + id;
